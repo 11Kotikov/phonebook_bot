5a212bd1fa6ce198e5463d1f3f4f8106c9c67856
Kotikov <11deadsluts@gmail.com>
2023-02-17 20:22:52 +0300

    trying to create full menu options

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import telebot
 from my_token import my_token
 from telebot import types
-import user_interface as ui
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+#import my modules
+import new_phone_book as newbook
+import user_interface as ui
+import contact_searching as cs
 bot=telebot.TeleBot(my_token)
 
-example = ''
+# example = ''
 
 calc_keyboard = InlineKeyboardMarkup(row_width=1)
 calc_keyboard.add(InlineKeyboardButton('New phone book (the old one will be deleted)', callback_data='0'),
@@ -23,6 +26,33 @@ calc_keyboard.add(InlineKeyboardButton('New phone book (the old one will be dele
 def calc_command(message: types.Message):
     bot.send_message(message.chat.id, 'This phone number book, hello dear "user_name"', reply_markup=calc_keyboard)
 
+@bot.callback_query_handler(lambda callback: callback.data)
+def control_system (callback: types.CallbackQuery):
+    if callback.data == '0':
+        newbook.new_clear_book()
+        bot.edit_message_text(f'You created a new clear book!', callback.message.chat.id, callback.message.id,
+                        reply_markup=calc_keyboard)
+    elif callback.data == '1':
+        get_state = cs.open_full_book()
+        if get_state == 0:
+            bot.edit_message_text(f'Your book is empty!', callback.message.chat.id, callback.message.id,
+                              reply_markup=calc_keyboard)
+        else:
+            with open('phone_book.csv') as pb:
+                while True:
+                    lines = pb.readline()
+                    if not lines:
+                        break
+                    bot.edit_message_text(f'{lines.strip()}', callback.message.chat.id, callback.message.id,
+                              reply_markup=calc_keyboard)
+    # elif callback.data == '1':
+    # elif callback.data == '1':
+    # elif callback.data == '1':
+    # elif callback.data == '1':
+    # elif callback.data == '1':
+    # elif callback.data == '1':
+    # elif callback.data == '1':
+    # else callback.data == '1':    
 
 
 bot.polling(none_stop=True)
